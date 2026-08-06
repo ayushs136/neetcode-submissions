8400b1383f70a7d4ce42412a9fb6dffb467c028b
AYUSH SHARMA <30320414+ayushs136@users.noreply.github.com>
2026-08-07 01:18:20 +0200

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+
+        adj = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+        res = []
+        for u, v in prerequisites:
+            adj[v].append(u)
+            indegree[u] += 1
+
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+
+        while q:
+            curr = q.popleft()
+            res.append(curr)
+            for node in adj[curr]:
+                indegree[node] -= 1
+                if indegree[node] == 0:
+                    q.append(node)
+
+        return res if len(res) == numCourses else []
